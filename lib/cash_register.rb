f1ce4e2320c86fb8b8f682acca40e7690b06e173
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-15 16:02:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount
+  @@total=0
+  
+  def initialize(discount=20)
+    @total=@@total
+      @discount
+  end 
 
+def self.discount (discount)
+  @discount = discount
+  discount=@total*(discount/100)
+end
+
+
+end 
